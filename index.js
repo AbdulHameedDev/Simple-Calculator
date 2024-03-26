3a8cc87f648daa0bfd24374072af7dc814756009
CodeWithHamza <Abdulhameeddev01@gmail.com>
2024-03-27 00:18:27 +0500

    Added Caclculator using Chalk

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
 // Printing a Wellcome Message
-console.log("\n\tWellcome To \'CodeWithHamza\' - CLI Simple Calculator\n");
+console.log(chalk.bold.rgb(204, 204, 204)(`\n  \t\t <<<================================================>>>`));
+console.log(chalk.bold.rgb(204, 204, 204)(`<<<=========>>>  ${chalk.redBright.bold('Wellcome To \'Code With Hamza\' - CLI Simple Calculator')}  <<<=========>>>`));
+console.log(chalk.bold.rgb(204, 204, 204)(`\t\t <<<===============================================>>>\n`));
+// console.log(chalk.bold.italic.bgYellow("\n\tWellcome To \'Code With Hamza\' - CLI Simple Calculator\n"));
 // Asking Questions from Users through Inquirer
 let answers = await inquirer.prompt([
-    { message: "Enter First Number", type: "number", name: "firstNumber" },
-    { message: "Enter Second Number", type: "number", name: "secondNumber" },
+    { message: chalk.magenta("Enter First Number"), type: "number", name: "firstNumber" },
+    { message: chalk.magenta("Enter Second Number"), type: "number", name: "secondNumber" },
     {
-        message: "Select one Operator to perform Operations",
+        message: chalk.green("Select one Operator to perform Operations"),
         type: "list",
         name: "operator",
         choices: ["Addition", "Subtractions", "Multiplication", "Division"],
@@ -27,5 +31,5 @@ else if (answers.operator === "Division") {
     console.log(answers.firstNumber / answers.secondNumber);
 }
 else {
-    console.log("Invalid Input");
+    console.log(chalk.red("Invalid Input"));
 }
